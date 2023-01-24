8546669da53fdfa2afc02bac8abf8cb9e2f50a26
MGliem <maximilian.gliem@gmail.com>
2023-01-24 22:28:48 +0100

    styling(frontend): dark theme

--- apps/frontend/web/src/App.tsx
+++ apps/frontend/web/src/App.tsx
@@ -1,13 +1,7 @@
-import {
-  Box,
-  Button,
-  Flex,
-  FormLabel,
-  Input,
-  Text,
-} from '@chakra-ui/react';
+import { Box, Button, Flex, FormLabel, Input, Text } from '@chakra-ui/react';
 import { useState } from 'react';
 import { io } from 'socket.io-client';
+import ChatRoom from './component/ChatRoom';
 
 const socket = io('', {
   path: '/chat-ws/',
@@ -25,45 +19,57 @@ function App() {
     }
   };
   return (
-    <Flex h={'100vh'} justify={'center'} alignItems={'center'}>
-      <Box
-        w={'50%'}
-        p={'12px'}
-        border={'1px solid #868686'}
-        rounded={'8'}
-        shadow={'lg'}
-      >
-        <Flex flexDir={'column'} alignItems={'center'}>
-          <Text as={'h1'} fontSize={'2rem'} marginBlockEnd={'32px'}>
-            {'Join a chat-room'}
-          </Text>
-          <FormLabel htmlFor={'name'} alignSelf={'start'}>
-            {'Name: '}
-          </FormLabel>
-          <Input
-            id="name"
-            marginBlockEnd={'24px'}
-            placeholder="Enter your name"
-            value={name}
-            onChange={(event) => {
-              setName(event.target.value);
-            }}
-          ></Input>
-          <FormLabel htmlFor={'room'} alignSelf={'start'}>
-            {'Room: '}
-          </FormLabel>
-          <Input
-            id="room"
-            marginBlockEnd={'24px'}
-            placeholder="Room ID..."
-            value={room}
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          ></Input>
-          <Button colorScheme='teal' isDisabled={name === '' && room === ''} onClick={joinRoom}>{'Enter'}</Button>
-        </Flex>
-      </Box>
+    <Flex h={'100vh'} justify={'center'} alignItems={'center'} backgroundColor={'#607485'}>
+      {!showChat ? (
+        <Box
+          w={'50%'}
+          p={'12px'}
+          border={'1px solid #ffffff'}
+          rounded={'8'}
+          shadow={'lg'}
+        >
+          <Flex flexDir={'column'} alignItems={'center'}>
+            <Text as={'h1'} fontSize={'2rem'} marginBlockEnd={'32px'} color={'white'}>
+              {'Join a chat-room'}
+            </Text>
+            <FormLabel htmlFor={'name'} alignSelf={'start'} color={'white'}>
+              {'Name: '}
+            </FormLabel>
+            <Input
+              id="name"
+              marginBlockEnd={'24px'}
+              placeholder="Enter your name"
+              backgroundColor={'white'}
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            ></Input>
+            <FormLabel htmlFor={'room'} alignSelf={'start'} color={'white'}>
+              {'Room: '}
+            </FormLabel>
+            <Input
+              id="room"
+              marginBlockEnd={'24px'}
+              placeholder="Room ID..."
+              backgroundColor={'white'}
+              value={room}
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }}
+            ></Input>
+            <Button
+              colorScheme="teal"
+              isDisabled={name === '' && room === ''}
+              onClick={joinRoom}
+            >
+              {'Enter'}
+            </Button>
+          </Flex>
+        </Box>
+      ) : (
+        <ChatRoom socket={socket} name={name} room={room} />
+      )}
     </Flex>
   );
 }
