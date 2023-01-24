e24d8baf80e97e7d3b79e18caf542df904b43e3a
MGliem <maximilian.gliem@gmail.com>
2023-01-24 21:29:19 +0100

    feat(backend): websocket connection and messages

---
+++ apps/backend/api/src/chat/chat.gateway.ts
@@ -0,0 +1,25 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway(8001, { path: '/chat-ws' })
+export class ChatGateway {
+  @WebSocketServer() wss: Server;
+
+  @SubscribeMessage('joinRoom')
+  handleRoomJoin(client: Socket, room: string) {
+    client.join(room);
+    client.emit('joinedRoom', room);
+  }
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(
+    client: Socket,
+    message: { sender: string; room: string; message: string },
+  ) {
+    this.wss.to(message.room).emit('chatToClient', message);
+  }
+}
